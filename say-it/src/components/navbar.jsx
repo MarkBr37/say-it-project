ab09bb1bcd0c4ffce6413b906aec94cc1d2446d9
Mark <mark.bruser37@gmail.com>
2022-06-23 21:15:10 +0300

    nav small change

--- say-it/src/components/navbar.jsx
+++ say-it/src/components/navbar.jsx
@@ -8,13 +8,13 @@ function NavBar({user}){
     return(
         <Navbar bg="dark" variant="dark" expand="md">
             <Container>
-                <NavLink className="navbar-brand" to="/">Say it</NavLink>
+                <NavLink className="navbar-brand" to="/home">Say it</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
 
                 <Nav className="me-auto">
 
-                    <NavLink className='nav-link' to="/">Home</NavLink>
+                    <NavLink className='nav-link' to="/home">Home</NavLink>
                     {user && <NavLink className='nav-link' to="/myposts">My Posts</NavLink>}
                     <NavLink className='nav-link' to="/about">About</NavLink>
 
