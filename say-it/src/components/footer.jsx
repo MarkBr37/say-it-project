aab45ed5a068b52ebc4ef9cfbdb7d69ff7cac47b
Mark <mark.bruser37@gmail.com>
2022-07-04 09:41:21 +0300

    fotter

--- say-it/src/components/footer.jsx
+++ say-it/src/components/footer.jsx
@@ -1,7 +1,7 @@
 const Fotter = () => {
     return ( 
        <div className="p-1 bg-dark mt-5">
-          <p className='pt-2 text-center text-light'>Just Drew &copy;{new Date().getFullYear()}</p>
+          <p className='pt-2 text-center text-light'>Say it &copy;{new Date().getFullYear()}</p>
        </div>
      );
  }
